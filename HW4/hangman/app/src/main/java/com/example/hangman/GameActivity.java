0e87740fbbddba9f3b69687010a4b06a5acf87f8
MaoMao <marvemao@bu.edu>
2022-02-23 14:57:06 -0500

    hangman edition2

--- HW4/hangman/app/src/main/java/com/example/hangman/GameActivity.java
+++ HW4/hangman/app/src/main/java/com/example/hangman/GameActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,12 +14,19 @@ public class GameActivity extends AppCompatActivity {
     public String answer;
     public ImageView imageView;
     public int errorCount;
+    public TextView gameResult;
+    public Button newGame;
+    public Boolean gameState;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
         wordtextView = (TextView) findViewById(R.id.wordtextView);
         imageView = (ImageView) findViewById(R.id.imageView);
+        gameResult = (TextView) findViewById(R.id.gameResult);
+        newGame = (Button) findViewById(R.id.newGame);
+        gameState = Boolean.TRUE;
+        gameResult.setVisibility(View.GONE);
         // we will set answer. currently, this is a word.
         // But later, we need to 1. set several word set for different field: cars, countries, etc.
         // 2. get word from different set randomly. set the word as answer below.
@@ -55,6 +63,26 @@ public class GameActivity extends AppCompatActivity {
 
         }
     }
+
+    public void initializedGame(View view){
+        // 1. error count.
+        errorCount = 0;
+        // 2. answer and textview
+        // the following is set answer and textview.
+        // TODO need change to random.
+        // remember to fit the length!!
+        answer = "RUSSIA";
+        wordtextView.setText("______");
+        // 3. image
+        imageView.setImageResource(R.drawable.p0);
+        // 4. game result
+        gameResult.setVisibility(View.GONE);
+        // 5. game state.
+        // only onCreate and initialize can change state to true (meaning: start game).
+        gameState = Boolean.TRUE;
+
+    }
+
     private void doJudge(String s){
         // first edition.
         // 1. click and change:
@@ -63,14 +91,31 @@ public class GameActivity extends AppCompatActivity {
         // third edition. based on second one, first check if exists.
         // if not exists: do change image.
         // else: do second condition.
+        // forth edition. check if current text view is the answer. if it is, return. Thus done nothing.
+        if(!gameState){
+            return;
+        }
+        if(wordtextView.getText().toString().compareTo(answer)==0){
+            gameResult.setText(R.string.game_result_win);
+            gameResult.setVisibility(View.VISIBLE);
+            // 5. gameState: end of the game: false
+            gameState = Boolean.FALSE;
+            return;
+        }// check if win. even after the game ends with win.
+        // Can be optimized using game state bool.
+
         if (answer.indexOf(current,0)==-1){
             errorCount += 1;
-            if(errorCount>=7){
+            changeImage();
+            if(errorCount>=6){
                 // TODO
                 // output defeat! like: set textview to visible.
+                gameResult.setText(R.string.game_result_loose);
+                gameResult.setVisibility(View.VISIBLE);
+                gameState = Boolean.FALSE;
                 return;
             }
-            changeImage();
+
         }else {
 
             String currentStr = (String) wordtextView.getText();
@@ -83,6 +128,12 @@ public class GameActivity extends AppCompatActivity {
                 flag = flag + 1;
             }
             wordtextView.setText(dupStr);
+            if(wordtextView.getText().toString().compareTo(answer)==0){
+                gameResult.setText(R.string.game_result_win);
+                gameResult.setVisibility(View.VISIBLE);
+                gameState = Boolean.FALSE;
+                return;
+            }// check game win after the input.
         }
 
     }
@@ -97,6 +148,150 @@ public class GameActivity extends AppCompatActivity {
             case R.id.b:
                 current = "B";
                 doJudge("B");
+                // count ++ ?
+                // do judge here?
+                break;
+            case R.id.c:
+                current = "C";
+                doJudge("C");
+                // count ++ ?
+                // do judge here?
+                break;
+            case R.id.d:
+                current = "D";
+                doJudge("D");
+                // count ++ ?
+                // do judge here?
+                break;
+            case R.id.e:
+                current = "E";
+                doJudge("E");
+                // count ++ ?
+                // do judge here?
+                break;
+            case R.id.f:
+                current = "F";
+                doJudge("F");
+                // count ++ ?
+                // do judge here?
+                break;
+            case R.id.g:
+                current = "G";
+                doJudge("G");
+                // count ++ ?
+                // do judge here?
+                break;
+            case R.id.h:
+                current = "H";
+                doJudge("H");
+                // count ++ ?
+                // do judge here?
+                break;
+            case R.id.i:
+                current = "I";
+                doJudge("I");
+                // count ++ ?
+                // do judge here?
+                break;
+            case R.id.j:
+                current = "J";
+                doJudge("J");
+                // count ++ ?
+                // do judge here?
+                break;
+            case R.id.k:
+                current = "K";
+                doJudge("K");
+                // count ++ ?
+                // do judge here?
+                break;
+            case R.id.l:
+                current = "L";
+                doJudge("L");
+                // count ++ ?
+                // do judge here?
+                break;
+            case R.id.m:
+                current = "M";
+                doJudge("M");
+                // count ++ ?
+                // do judge here?
+                break;
+            case R.id.n:
+                current = "N";
+                doJudge("N");
+                // count ++ ?
+                // do judge here?
+                break;
+            case R.id.o:
+                current = "O";
+                doJudge("O");
+                // count ++ ?
+                // do judge here?
+                break;
+            case R.id.p:
+                current = "P";
+                doJudge("P");
+                // count ++ ?
+                // do judge here?
+                break;
+            case R.id.q:
+                current = "Q";
+                doJudge("Q");
+                // count ++ ?
+                // do judge here?
+                break;
+            case R.id.r:
+                current = "R";
+                doJudge("R");
+                // count ++ ?
+                // do judge here?
+                break;
+            case R.id.s:
+                current = "S";
+                doJudge("S");
+                // count ++ ?
+                // do judge here?
+                break;
+            case R.id.t:
+                current = "T";
+                doJudge("T");
+                // count ++ ?
+                // do judge here?
+                break;
+            case R.id.u:
+                current = "U";
+                doJudge("U");
+                // count ++ ?
+                // do judge here?
+                break;
+            case R.id.v:
+                current = "V";
+                doJudge("V");
+                // count ++ ?
+                // do judge here?
+                break;
+            case R.id.w:
+                current = "W";
+                doJudge("W");
+                // count ++ ?
+                // do judge here?
+                break;
+            case R.id.x:
+                current = "X";
+                doJudge("X");
+                // count ++ ?
+                // do judge here?
+                break;
+            case R.id.y:
+                current = "Y";
+                doJudge("Y");
+                // count ++ ?
+                // do judge here?
+                break;
+            case R.id.z:
+                current = "Z";
+                doJudge("Z");
                 // count ++ ?
                 // do judge here?
                 break;
