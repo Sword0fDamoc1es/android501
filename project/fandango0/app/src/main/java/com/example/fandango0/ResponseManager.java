9eebe0bdce6eee96b6b49d86ac5915cdb294d1c8
Ivan Mata <60793608+imata18@users.noreply.github.com>
2022-03-28 16:57:37 -0400

    Add files via upload

--- project/fandango0/app/src/main/java/com/example/fandango0/ResponseManager.java
+++ project/fandango0/app/src/main/java/com/example/fandango0/ResponseManager.java
@@ -1,9 +1,17 @@
 package com.example.fandango0;
 
 import android.content.Context;
+
+import com.example.fandango0.model.APIresponse;
+
+import java.util.List;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
+import retrofit2.Call;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public class ResponseManager {
     Context context;
@@ -16,11 +24,12 @@ public class ResponseManager {
     }
 
     public interface AMC_locationSuggestion_zip{
-        @GET("location-suggestions/?query={zip}")
+        @GET("location-suggestions/?query={77429}")
+        Call<List<String>> repos(
+               @Path("77429") String word
+        );
 
 
     }
-
-    @Override
 
 }
