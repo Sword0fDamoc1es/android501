5a19b77435f8bd0840145c683cad40bf1f545f82
MaoMao <marvemao@bu.edu>
2022-04-19 00:17:49 -0400

    sign up

--- project/Meet4Sho/app/src/main/java/com/example/meet4sho/loginActivity.java
+++ project/Meet4Sho/app/src/main/java/com/example/meet4sho/loginActivity.java
@@ -2,14 +2,19 @@ package com.example.meet4sho;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentContainerView;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -21,30 +26,74 @@ public class loginActivity extends AppCompatActivity {
     public EditText edtPassword;
     public EditText edtUsername;
     public Button btnLogin;
+    public Button btnSignUp;
+
+    // MARVE: comment
+    private DocumentReference pDocRef = FirebaseFirestore.getInstance().document("front_end/user");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
-        userNames.put("Test", "123");
+        edtPassword = findViewById(R.id.signup_pwd);
+        edtUsername = findViewById(R.id.signup_name);
+        btnLogin = findViewById(R.id.btnCreate);
 
-        edtPassword = findViewById(R.id.edtPassword);
-        edtUsername = findViewById(R.id.edtUsername);
-        btnLogin = findViewById(R.id.btnLogin);
-
-        btnLogin.setOnClickListener(new View.OnClickListener() {
+        btnSignUp = findViewById(R.id.btnSignUp);
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(userNames.containsKey(edtUsername.getText().toString())) {
-                    if(userNames.get(edtUsername.getText().toString()).equals(edtPassword.getText().toString())){
-                        Intent intent = new Intent(loginActivity.this, AppActivity.class);
-                        intent.putExtra("username",edtUsername.getText().toString());
-                        startActivity(intent);
-                    }
-                }
+                Intent i = new Intent(getApplicationContext(),signupActivity.class);
+                startActivity(i);
             }
         });
 
+
+
+        userNames.put("Test", "123");
+
+
+//        btnLogin.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if(userNames.containsKey(edtUsername.getText().toString())) {
+//                    if(userNames.get(edtUsername.getText().toString()).equals(edtPassword.getText().toString())){
+//                        Intent intent = new Intent(loginActivity.this, AppActivity.class);
+//                        intent.putExtra("username",edtUsername.getText().toString());
+//                        startActivity(intent);
+//                    }
+//                }
+//            }
+//        });
+
+    }
+    public void onLogin(View view){
+        String uname = edtUsername.getText().toString();
+        String upwd = edtPassword.getText().toString();
+        DocumentReference docRef = pDocRef.collection("user").document(uname);
+        DocumentSnapshot document;
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d("GET", "DocumentSnapshot data: " + document.getData().get("uid"));
+                        if (document.getData().get("uid").toString().equals(uname)&&document.getData().get("upwd").toString().equals(upwd)){
+                            Intent i = new Intent(getApplicationContext(),AppActivity.class);
+                            // input here
+                            i.putExtra("username",edtUsername.getText().toString());
+                            startActivity(i);
+
+                        }
+                    } else {
+                        Log.d("not GET", "No such document");
+                    }
+                } else {
+                    Log.d("Failed", "get failed with ", task.getException());
+                }
+            }
+        });
     }
 
 }
