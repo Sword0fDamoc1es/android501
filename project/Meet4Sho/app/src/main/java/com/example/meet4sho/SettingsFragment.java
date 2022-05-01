d7fab22cf25129ce0e8e10f039ae97f809df23c5
Chang Liu <liuchangterry1999@gmail.com>
2022-05-01 00:04:09 -0400

    Update fragment_settings.xml

--- project/Meet4Sho/app/src/main/java/com/example/meet4sho/SettingsFragment.java
+++ project/Meet4Sho/app/src/main/java/com/example/meet4sho/SettingsFragment.java
@@ -33,6 +33,6 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.messages_page, container, false);
+        return inflater.inflate(R.layout.fragment_settings, container, false);
     }
 }
